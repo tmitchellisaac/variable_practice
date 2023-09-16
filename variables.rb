7283fe8f857ada762170a7f70667bf3067348b9f
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-09-16 15:36:21 -0600

    Change string variable contents, add put statements

--- variables.rb
+++ variables.rb
@@ -1,6 +1,6 @@
-email = "tmitchellisaac@gmail.com"
-name = "Isaac Mitchell"
-city_lived_in = "Denver"
+email = "mitchell.isaac@yahoo.com"
+name = "John Doe"
+city_lived_in = "Wheat Ridge"
 
 age = 32
 cars_owned = 2
@@ -8,10 +8,20 @@ roommates = 1
 limbs = 4
 
 dollars_in_wallet = 23.5
-beers_drank_today 0.5
+beers_drank_today = 0.5
 liters_of_water_drank_today = 2.5
 
-hungry_rn_? = true
+hungry_rn = true
 wearing_shoes = false
 happy = true
 almost_done = true
+
+puts email
+
+puts name 
+
+puts age
+
+puts beers_drank_today
+
+puts dollars_in_wallet
